9eab7ccc47ffeac1a4b8e70c1891ee6df3ebbe72
Lara-Codes <lara.palombi@gmail.com>
2024-05-04 17:45:27 -0400

    commented queue.py

--- server/Classes/Queue.py
+++ server/Classes/Queue.py
@@ -3,44 +3,43 @@ from flask import jsonify, current_app
 
 
 class Queue:
-    # Only adding ID to the queue
     def __init__(self):
         self.__queue = deque()  # use Python double-ended queue
 
     def __iter__(self):  # iterate over queue
         return iter(self.__queue)
-    
-    # def setToInQueue(self): 
-    #     for job in self.__queue: 
-    #         job.status = "inqueue"
 
-    # if no priority add to end of queue. If priority add to front of queue.
+    """
+        Add job to back of printer queue 
+    """
     def addToBack(self, job, printerid):
         if self.__queue.count(job) > 0:
             raise Exception("Job ID already in queue.")
         self.__queue.append(
             job
-        )  # appending on the right is the "front" because popping takes out from right d
+        )  
         current_app.socketio.emit(
             "queue_update", {"queue": self.convertQueueToJson(), "printerid": printerid}
         )
 
+    """
+        Add job to front of printer queue or to second posiion if first job is printing
+    """
     def addToFront(self, job, printerid):
         if self.__queue.count(job) > 0:
             raise Exception("Job ID already in queue.")
-        # If the queue has at least one job and the first job is printing,
-        # insert at the second position because we don't want to interrupt it.
         if len(self.__queue) >= 1 and self.__queue[0].status == "printing":
             self.__queue.insert(1, job)
-        # If the queue is empty or the first job is not printing,
-        # add the job to the front
         else:
             self.__queue.appendleft(job)
         current_app.socketio.emit(
             "queue_update", {"queue": self.convertQueueToJson(), "printerid": printerid}
         )
 
-    def bump(self, up, jobid):  # up = boolean. if up = true bump up, else bump down
+    """
+        Unsure if we use this function, used to change position of jobs in queue 
+    """
+    def bump(self, up, jobid): 
         index = next(
             (
                 index
@@ -59,8 +58,10 @@ class Queue:
         elif not up and index < len(self.__queue):
             self.__queue.insert(index + 1, job_to_move)
         
+    """
+        Used to reorder the queue based on the order of the jobids in the arr
+    """
     def reorder(self, arr): 
-        # arr is an array of job ids in the order they should be in the queue
         new_queue = deque()
         for jobid in arr: 
             for job in self.__queue: 
@@ -69,6 +70,9 @@ class Queue:
                     break
         self.__queue = new_queue
     
+    """
+        Delete job from queue
+    """
     def deleteJob(self, jobid, printerid):
         deletedjob = None
         for job in self.__queue:
@@ -82,9 +86,11 @@ class Queue:
                 return deletedjob
         return "Job not found in queue."
 
+    """
+        Convery queue data to json format to send to frontend by other functions
+    """
     def convertQueueToJson(self):
         queue = []
-        # job_info = {}
         for job in self.__queue:
             job_info = {
                 "id": job.id,
@@ -111,7 +117,10 @@ class Queue:
             queue.append(job_info)
         return queue
 
-    def bumpExtreme(self, front, jobid, printerid):  # bump to back/front of queue
+    """
+        Do we use this? front parameter is a boolean, sends job to absolute end of queue or absolute beginning 
+    """
+    def bumpExtreme(self, front, jobid, printerid): 
         index = next(
             (
                 index
@@ -126,35 +135,49 @@ class Queue:
         job_to_move = self.__queue[index]
         self.__queue.remove(job_to_move)
         if front == True:
-            # If the queue has at least one job and the first job is printing,
-            # insert at the second position because we don't want to interrupt it.
             if len(self.__queue) >= 1 and self.__queue[0].status == "printing":
                 self.__queue.insert(1, job_to_move)
-            # If the queue is empty or the first job is not printing,
-            # add the job to the front
             else:
                 self.__queue.insert(0, job_to_move)
         else:
             self.addToBack(job_to_move, printerid)
 
+    """ 
+        Find and return job in queue based on job PK 
+    """
     def getJob(self, job_to_find):
         for job in self.__queue:
             if job.getJobId() == job_to_find.getJobId():
                 return job
-        return None  # Return None if job is not found in the queue
+        return None  
 
+    """
+        Same as above? Not sure why we have two of these functions / which one we use 
+    """
     def getJobById(self, job_to_find):
         for job in self.__queue:
             if job.getJobId() == job_to_find:
                 return job
-        return None  # Return None if job is not found in the queue
+        return None  
 
+    """
+        Says if job exists in queue or not
+    """
     def jobExists(self, jobid):
         for job in self.__queue:
             if job.id == jobid:
                 return True
         return False
 
+    """
+        Removes last job in queue
+    """
+    def removeJob(self):
+        self.__queue.pop()
+        
+    """
+       Getters  
+    """
     def getQueue(self):
         return self.__queue
 
@@ -164,6 +187,3 @@ class Queue:
     def getSize(self):
         return len(self.__queue)
 
-    def removeJob(self):
-        self.__queue.pop()
-        # current_app.socketio.emit('job_removed', {'queue': list(self.__queue)}, broadcast=True)
