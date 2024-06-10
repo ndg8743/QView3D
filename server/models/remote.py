5017bad2f45adfc407e5848b14f425e893ca5ed8
currymike <currymike@gmail.com>
2024-06-10 11:26:14 -0400

    Updated the remote server to 260 lab

--- server/models/remote.py
+++ server/models/remote.py
@@ -1,14 +1,18 @@
 import os
 from dotenv import load_dotenv, set_key
 
-local = "http://localhost:8000"
-remote = "https://my-remote-server.com"
-
 # Load the existing .env file. 
 load_dotenv("server/.env")
 
-# Set the new BASE_URL
-set_key("server/.env", "BASE_URL", local)
+local = "http://localhost:8000"
+# Remote server running on 192.168.1.106 port 8000
+remote = "http://192.168.1.106:8000"
+current = os.getenv("BASE_URL")
 
-print(f"BASE_URL updated to {local}")
+#Check to see if the user wants to write to the db locally or remotely. Get flag from the user.
+
+# Set the new BASE_URL
+set_key("server/.env", "BASE_URL", remote)
+
+print(f"BASE_URL updated to {remote}")
 
